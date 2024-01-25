0e277690fe19e2d37fa092f4314662fb806574ea
Ashwin2207 <ashwins2207@gmail.com>
2024-01-25 15:36:24 +0530

    two

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,43 @@
+// import { BrowserRouter,Routes,Route } from "react-router-dom"
+// import Layout from "./Layout"
+// import Home from "./Pages/Home"
+// import About from "./Pages/About"
+// import Contact from "./Pages/Contact"
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Layout/>}>
+//         <Route index element={<Home/>}/>
+//         <Route path="about" element={<About/>}/>
+//         <Route path="contact" element={<Contact/>}/>
+//       </Route>
+//     </Routes>
+//     </BrowserRouter>
+//   )
+// }
+// import React from 'react'
+// import Basic from './Basic'
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Basic/>
+//     </div>
+//   )
+// }
 import React from 'react'
-import Ter from './Ter'
+import Counter from './Counter'
 
 export default function App() {
   return (
     <div>
-      <Ter></Ter>
+      <Counter/>
     </div>
   )
 }
+
+
 
 
 
